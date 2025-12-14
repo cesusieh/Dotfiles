1319a148b108775971cbe3acf273e881aee1c990
cesusieh <cesux3@gmail.com>
2025-12-14 17:16:25 -0300

    Gaming module

---
+++ System/Modules/gaming.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+
+{
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; 
+    dedicatedServer.openFirewall = true;
+    gamescopeSession.enable = true;
+  };
+
+  programs.gamemode.enable = true;
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true; 
+  };
+  services.xserver.videoDrivers = [ "amdgpu" ];
+}
