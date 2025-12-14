90d8f3afe6933be956c9258cf38d213caa60fe39
cesusieh <cesux3@gmail.com>
2025-12-14 17:12:19 -0300

    Mount storage device

---
+++ System/Modules/storage.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+
+{
+  fileSystems."/home/carlinhos/HD" = { 
+    device = "/dev/disk/by-uuid/8e1dbbc0-86be-4649-8960-01b48eddd9bd";
+    fsType = "ext4";
+    options = [ "defaults" "nofail" ];
+  };
+}
