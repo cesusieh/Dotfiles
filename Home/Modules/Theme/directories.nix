d8a4801b63b2ea8851b2cb949bdcced884737318
cesusieh <cesux3@gmail.com>
2025-12-21 04:33:10 -0300

    Config xdg directories

---
+++ Home/Modules/Theme/directories.nix
@@ -0,0 +1,18 @@
+{ config, ... }:
+
+{
+  xdg.userDirs = {
+
+    enable = true;
+    createDirectories = true;
+
+    desktop = "${config.home.homeDirectory}/Área de Trabalho";
+    documents = "${config.home.homeDirectory}/Documentos";
+    download = "${config.home.homeDirectory}/Downloads";
+    music = "${config.home.homeDirectory}/Música";
+    pictures = "${config.home.homeDirectory}/Imagens";
+    videos = "${config.home.homeDirectory}/Vídeos";
+    templates = "${config.home.homeDirectory}/Modelos";
+    publicShare = "${config.home.homeDirectory}/Público";
+  };
+}
